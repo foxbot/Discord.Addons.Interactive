11e5623e890b336eeb552960c74fdf6f3b7654b3
iCrushDreams <29741584+iCrushDreams@users.noreply.github.com>
2018-12-30 22:25:23 -0500

    feature: [min] CancellationToken support for NextMessageAsync (#4)

--- Discord.Addons.Interactive/InteractiveBase.cs
+++ Discord.Addons.Interactive/InteractiveBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -15,10 +16,10 @@ namespace Discord.Addons.Interactive
     {
         public InteractiveService Interactive { get; set; }
 
-        public Task<SocketMessage> NextMessageAsync(ICriterion<SocketMessage> criterion, TimeSpan? timeout = null)
-            => Interactive.NextMessageAsync(Context, criterion, timeout);
-        public Task<SocketMessage> NextMessageAsync(bool fromSourceUser = true, bool inSourceChannel = true, TimeSpan? timeout = null) 
-            => Interactive.NextMessageAsync(Context, fromSourceUser, inSourceChannel, timeout);
+        public Task<SocketMessage> NextMessageAsync(ICriterion<SocketMessage> criterion, TimeSpan? timeout = null, CancellationToken token = default(CancellationToken))
+            => Interactive.NextMessageAsync(Context, criterion, timeout, token);
+        public Task<SocketMessage> NextMessageAsync(bool fromSourceUser = true, bool inSourceChannel = true, TimeSpan? timeout = null, CancellationToken token = default(CancellationToken)) 
+            => Interactive.NextMessageAsync(Context, fromSourceUser, inSourceChannel, timeout, token);
 
         public Task<IUserMessage> ReplyAndDeleteAsync(string content, bool isTTS = false, Embed embed = null, TimeSpan? timeout = null, RequestOptions options = null)
             => Interactive.ReplyAndDeleteAsync(Context, content, isTTS, embed, timeout, options);
